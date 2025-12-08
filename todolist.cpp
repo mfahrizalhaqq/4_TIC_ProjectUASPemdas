a1137b197d71f16bcd5754dfeea89437606c1a8d
Arva Josi <josiarva75517@gmail.com>
2025-12-08 22:09:25 +0700

    pembetulan codingan, menambahkan void dan class

--- todolist.cpp
+++ todolist.cpp
@@ -1,88 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <ctime>    
 using namespace std;
 
 struct tugas{
-    string nama;
-    string keterangan;
-    string deadline;
+    string NamaTugas;
+    string Keterangan;
+    string Deadline;
+    string kategori;
 };
-    tugas daftar[100];
-    int jumlahTugas = 0;
 
-int loadfile() {
-    ifstream file("tugas.txt");
-    if (!file.is_open()) return 0;
 
-    jumlahTugas = 0;
-    while (getline(file, daftar[jumlahTugas].nama)) {
-    getline(file, daftar[jumlahTugas].deadline);
-    getline(file, daftar[jumlahTugas].keterangan);
-    jumlahTugas++;
-}
-    file.close();
-}
-int simpanfile() {
-    ofstream file("tugas.txt");
-    for (int i = 0; i < jumlahTugas; i++) {
-        file << daftar[i].nama << endl;
-        file << daftar[i].deadline << endl;
-        file << daftar[i].keterangan << endl;
-    }
-    file.close();
-}
-int tambahTugas() {
+void inputtugas(tugas *t){
     cout << "Masukkan nama tugas: ";
     cin.ignore();
-    getline(cin, daftar[jumlahTugas].nama);
+    getline(cin, t->NamaTugas);
 
     cout << "Masukkan deadline tugas: ";
-    getline(cin, daftar[jumlahTugas].deadline);
+    getline(cin, t->Deadline);
 
     cout << "Masukkan keterangan tugas: ";
-    getline(cin, daftar[jumlahTugas].keterangan);
+    getline(cin, t->Keterangan);
 
-    jumlahTugas++;
-    simpanfile();
-    cout << "\ntugas berhasil ditambahkan!\n";
+    cout << "Masukkan kategori tugas: ";
+    getline(cin, t->kategori);
 }
-int lihattugas() {
-    cout << "\ndaftar tugas:\n";
-     if (jumlahTugas == 0) {
-        cout << "Belum ada tugas.\n";
-        return;
-    }
 
-    tugas* ptr = daftar;
-
-    for (int i = 0; i < jumlahTugas; i++) {
-        cout << "\nTugas ke-" << (i + 1) << endl;
-        cout << "Nama      : " << ptr[i].nama << endl;
-        cout << "Deadline  : " << ptr[i].deadline << endl;
-        cout << "Keterangan: " << ptr[i].keterangan << endl;
-    }
-}
-int hapusTugas() {
-    lihattugas();
-    if (jumlahTugas == 0) {
-        return;
-    }
-    int index;
-    cout << "\nMasukkan nomor tugas yang ingin dihapus: ";
-    cin >> index;
-    index--;
-
-    if (index < 0 || index >= jumlahTugas) {
-        cout << "Nomor tidak valid!\n";
-        return;
-    }
-    for (int i = index; i < jumlahTugas - 1; i++) {
-        daftar[i] = daftar[i + 1];
-    }
-    jumlahTugas--;
-    simpanfile();
-
-    cout << "\nTugas berhasil dihapus!\n";
+void tampilkantugas(const tugas *t){
+    cout << "Nama Tugas: " << t->NamaTugas << endl;
+    cout << "Deadline: " << t->Deadline << endl;
+    cout << "Keterangan: " << t->Keterangan << endl;
+    cout << "Kategori: " << t->kategori << endl;
 }
 
